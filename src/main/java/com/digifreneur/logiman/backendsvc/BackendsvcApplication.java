84f73864c1a05461ea7481c3c52b1688602105ba
Sigit Prayitno <cybergitt@gmail.com>
2021-08-17 18:23:47 +0700

    make it better

--- src/main/java/com/digifreneur/logiman/backendsvc/BackendsvcApplication.java
+++ src/main/java/com/digifreneur/logiman/backendsvc/BackendsvcApplication.java
@@ -2,6 +2,8 @@ package com.digifreneur.logiman.backendsvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class BackendsvcApplication {
@@ -10,4 +12,11 @@ public class BackendsvcApplication {
 		SpringApplication.run(BackendsvcApplication.class, args);
 	}
 
+	@RestController
+	class RandomResource {
+		@GetMapping(path = "/greet")
+		public String greet() {
+			return String.format("%s says Hello from %s!", "Spring Boot", "Heroku");
+		}
+	}
 }
